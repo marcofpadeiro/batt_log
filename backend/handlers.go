092082b5dfe19b84b5a3fc2008a88cd76e0651a7
marcofpadeiro <marcofpadeiro@gmail.com>
2024-04-04 00:37:05 +0100

    Refactoring backend handlers code to be more readable and maintainable.     - Adding row parsers to the handlers     - Using INNER JOIN to get detailed sessions instead of using multiple queries (Should improve performance)

--- backend/handlers.go
+++ backend/handlers.go
@@ -11,8 +11,7 @@ import (
 
 func getSessions(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Query to get all sessions
-		rows, err := db.Query("SELECT id, session_type FROM session ORDER BY id DESC")
+		rows, err := db.Query("SELECT * FROM event INNER JOIN session ON event.session_id = session.id ORDER BY event.timestamp ASC")
 		if err != nil {
 			log.Printf("Error querying sessions: %v\n", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -20,89 +19,14 @@ func getSessions(db *sql.DB) gin.HandlerFunc {
 		}
 		defer rows.Close()
 
-		var sessions []Session
-
-		for rows.Next() {
-			var s Session
-			if err := rows.Scan(&s.ID, &s.SessionType); err != nil {
-				log.Printf("Error scanning sessions: %v\n", err)
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-			sessions = append(sessions, s)
-		}
-
-		var detailedSessions []DetailedSession
-
-		for _, session := range sessions {
-			var events []Event
-			eventRows, err := db.Query("SELECT * FROM event WHERE session_id = ? ORDER BY timestamp ASC", session.ID)
-			if err != nil {
-				log.Printf("Error querying events: %v\n", err)
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-
-			var totalPower int
-
-			for eventRows.Next() {
-				var e Event
-				if err := eventRows.Scan(&e.ID, &e.SessionID, &e.Timestamp, &e.Capacity, &e.PowerDraw); err != nil {
-					log.Printf("Error scanning events: %v\n", err)
-					eventRows.Close() // Close the eventRows before returning
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-				}
-				totalPower += e.PowerDraw
-				events = append(events, e)
-			}
-			eventRows.Close()
-
-			if len(events) > 0 {
-				start := events[0].Timestamp
-				end := events[len(events)-1].Timestamp
-				duration := calculateDuration(start, end)
-				capacityStart := events[0].Capacity
-				capacityEnd := events[len(events)-1].Capacity
-				capacityDelta := capacityEnd - capacityStart
-				averagePower := totalPower / len(events)
-
-				detailedSession := DetailedSession{
-					ID:                        session.ID,
-					SessionType:               session.SessionType,
-					Start:                     start,
-					End:                       end,
-					Duration:                  duration,
-					Capacity_Start:            capacityStart,
-					Capacity_End:              capacityEnd,
-					Capacity_Delta:            capacityDelta,
-					Average_Power_Consumption: averagePower,
-				}
-
-				detailedSessions = append(detailedSessions, detailedSession)
-			}
-		}
-
-		c.JSON(http.StatusOK, detailedSessions)
-	}
-}
-
-func getSession(db *sql.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var session Session
-		id := c.Param("id")
-
-		err := db.QueryRow("SELECT * FROM session WHERE id = ?", id).Scan(&session.ID, &session.SessionType)
-
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Session not found"})
-			return
-		} else if err != nil {
+		detailedSessions, err := parseRowsIntoDetailedSessions(rows)
+		if err != nil {
+			log.Printf("Error parsing rows into sessions: %v\n", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(http.StatusOK, session)
+		c.JSON(http.StatusOK, detailedSessions)
 	}
 }
 
@@ -116,19 +40,21 @@ func getEvents(db *sql.DB) gin.HandlerFunc {
 		}
 		defer rows.Close()
 
-		var events []Event
-
-		for rows.Next() {
-			var temp Event
-			if err := rows.Scan(&temp.ID, &temp.SessionID, &temp.Timestamp, &temp.Capacity, &temp.PowerDraw); err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-			events = append(events, temp)
-		}
+		events, err := parseRowsIntoEvents(rows)
 
 		c.JSON(http.StatusOK, events)
+	}
+}
+
+func getSession(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var session Session
+		id := c.Param("id")
+
+		err := db.QueryRow("SELECT * FROM session WHERE id = ?", id).Scan(&session.ID, &session.SessionType)
+		handleSingleQueryErr(err, c)
+
+		c.JSON(http.StatusOK, session)
 	}
 }
 
@@ -138,14 +64,7 @@ func getEvent(db *sql.DB) gin.HandlerFunc {
 		id := c.Param("id")
 
 		err := db.QueryRow("SELECT * FROM event WHERE id = ?", id).Scan(&event.ID, &event.SessionID, &event.Timestamp, &event.Capacity, &event.PowerDraw)
-
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
-			return
-		} else if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+		handleSingleQueryErr(err, c)
 
 		c.JSON(http.StatusOK, event)
 	}
@@ -162,16 +81,11 @@ func getSessionEvents(db *sql.DB) gin.HandlerFunc {
 		}
 		defer rows.Close()
 
-		var events []Event
-
-		for rows.Next() {
-			var temp Event
-			if err := rows.Scan(&temp.ID, &temp.SessionID, &temp.Timestamp, &temp.Capacity, &temp.PowerDraw); err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-			events = append(events, temp)
+		events, err := parseRowsIntoEvents(rows)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
 		c.JSON(http.StatusOK, events)
--- backend/lib.go
+++ backend/lib.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Session struct {
-	ID          int    `json:"id"`
-	SessionType string `json:"session_type"`
+	ID          int     `json:"id"`
+	SessionType string  `json:"session_type"`
+	Events      []Event `json:"events"`
 }
 
 type Event struct {
@@ -30,8 +35,77 @@ type DetailedSession struct {
 	Average_Power_Consumption int    `json:"average_power_consumption"`
 }
 
+func parseRowsIntoEvents(rows *sql.Rows) ([]Event, error) {
+	var events []Event
+
+	for rows.Next() {
+		var temp Event
+		if err := rows.Scan(&temp.ID, &temp.SessionID, &temp.Timestamp, &temp.Capacity, &temp.PowerDraw); err != nil {
+			return nil, err
+		}
+		events = append(events, temp)
+	}
+
+	return events, nil
+}
+
+func parseRowsIntoDetailedSessions(rows *sql.Rows) ([]DetailedSession, error) {
+	var detailedSessions []DetailedSession
+
+	sessionsMap, err := mapEventsToSessions(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, session := range sessionsMap {
+		detailedSessions = append(detailedSessions, convertSessionToDetailedSession(session))
+	}
+
+	return detailedSessions, nil
+}
+
+func mapEventsToSessions(rows *sql.Rows) (map[int]*Session, error) {
+	sessionsMap := make(map[int]*Session)
+
+	for rows.Next() {
+		var event Event
+		var session Session
+
+		if err := rows.Scan(&event.ID, &event.SessionID, &event.Timestamp, &event.Capacity, &event.PowerDraw, &session.ID, &session.SessionType); err != nil {
+			return nil, err
+		}
+
+		if tempSession, exists := sessionsMap[session.ID]; exists {
+			tempSession.Events = append(tempSession.Events, event)
+		} else {
+			newSession := &Session{
+				ID:          session.ID,
+				SessionType: session.SessionType,
+				Events:      []Event{event},
+			}
+			sessionsMap[session.ID] = newSession
+		}
+	}
+
+	return sessionsMap, nil
+}
+
+func convertSessionToDetailedSession(session *Session) DetailedSession {
+	return DetailedSession{
+		ID:                        session.ID,
+		SessionType:               session.SessionType,
+		Start:                     session.Events[0].Timestamp,
+		End:                       session.Events[len(session.Events)-1].Timestamp,
+		Duration:                  calculateDuration(session.Events[0].Timestamp, session.Events[len(session.Events)-1].Timestamp),
+		Capacity_Start:            session.Events[0].Capacity,
+		Capacity_End:              session.Events[len(session.Events)-1].Capacity,
+		Capacity_Delta:            session.Events[len(session.Events)-1].Capacity - session.Events[0].Capacity,
+		Average_Power_Consumption: calculateAveragePower(session.Events),
+	}
+}
+
 func calculateDuration(start string, end string) int {
-	const timeLayout = "2006-01-02T15:04:05Z" // ISO 8601 format; change this to match your actual time format
+	const timeLayout = "2006-01-02T15:04:05Z"
 	startTime, err := time.Parse(timeLayout, start)
 	if err != nil {
 		log.Printf("Error parsing start time: %v\n", err)
@@ -45,4 +119,23 @@ func calculateDuration(start string, end string) int {
 
 	duration := endTime.Sub(startTime)
 	return int(duration.Seconds())
+}
+
+func calculateAveragePower(events []Event) int {
+	totalPower := 0
+	for _, event := range events {
+		totalPower += event.PowerDraw
+	}
+
+	return totalPower / len(events)
+}
+
+func handleSingleQueryErr(err error, c *gin.Context) {
+	if err == sql.ErrNoRows {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 }
