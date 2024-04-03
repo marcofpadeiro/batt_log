b9fa311e373a5aa30cc12634cbfd465ab6842dce
Marco Padeiro <marcofpadeiro@gmail.com>
2024-04-03 22:01:25 +0100

    Refactoring to ease the addition of new services and readibility

--- service/install.sh
+++ service/install.sh
@@ -5,46 +5,49 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-INIT=$(ps --no-headers -o comm 1)
+if [ ! -x "$(command -v cargo)" ]; then
+    echo "Cargo is not installed. Please install Rust and Cargo before running this script."
+    exit 1
+fi
 
-chmod +x services/systemd/batt_log.service
-chmod +x services/runit/run
+# Will either be systemd, runit, or init
+INIT_SYSTEM=$(ps --no-headers -o comm 1)
 
-cargo build --release
-
-if [ $? -ne 0 ]; then
+if ! cargo build --release; then
     echo "Cargo build failed, exiting."
     exit 1
 fi
 
 cp target/release/batt_log /usr/local/bin/
 
-echo "Installed batt_log to /usr/local/bin/"
+echo "Installed batt_log to /usr/local/bin/batt_log"
 echo "You can now run batt_log from the terminal."
 
-echo "Detected init: $INIT"
-read -p "Do you want to install batt_log as a service? [Y/n]: " install_service
-
-if [ "$install_service" = "" ]; then
-    install_service="y"
-fi
-
-if [ "$install_service" != "y" ]; then
+echo "Detected init: $INIT_SYSTEM"
+read -p "Do you want to install batt_log as a service? [Y/n]:" install_service
+    
+if [[ "${install_service,,}" =~ ^(y|)$ ]]; then
     exit 0
 fi
 
-if [ "$INIT" = "systemd" ]; then 
-    cp services/systemd/batt_log.service /etc/systemd/system/
+declare -A services
+services=(["systemd"]="systemd" ["runit"]="runit")
 
+systemd() {
+    chmod +x services/systemd/batt_log.service
+    cp services/systemd/batt_log.service /etc/systemd/system/
     systemctl daemon-reload
     systemctl enable batt_log
     systemctl restart batt_log
-elif [ "$INIT" = "runit"  ];
-then
+}
+
+runit() {
+    chmod +x services/runit/run
     cp -r services/runit /etc/sv/batt_log
     ln -sv /etc/sv/batt_log /var/service
     sv up batt_log
-fi
+}
 
+${services[$INIT_SYSTEM]}
 
 echo "Installed batt_log as a service successfully."
