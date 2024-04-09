4e18473de4ed442e59fe588a4df50c193bf1a1b3
marcofpadeiro <marcofpadeiro@gmail.com>
2024-04-09 15:38:52 +0100

    Base TUI interface doneTODO: write renders for info, list, graph and modeTODO: make queries to get sessions

---
+++ tui/src/app.rs
@@ -0,0 +1,86 @@
+use std::error::Error;
+
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{
+    backend::Backend,
+    buffer::Buffer,
+    layout::{Constraint, Layout, Rect},
+    widgets::{Paragraph, Widget},
+    Terminal,
+};
+use tui::SessionList;
+
+pub struct App {
+    items: SessionList,
+}
+
+impl App {
+    pub fn new() -> Self {
+        //Self {
+        todo!("Query database");
+        //}
+    }
+
+    pub fn run(&mut self, mut terminal: Terminal<impl Backend>) -> Result<(), Box<dyn Error>> {
+        loop {
+            self.draw(&mut terminal)?;
+
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    use KeyCode::*;
+                    match key.code {
+                        Char('q') | Esc => return Ok(()),
+                        Char('j') | Down => self.items.next(),
+                        Char('k') | Up => self.items.previous(),
+                        Char('g') => self.items.go_top(),
+                        Char('G') => self.items.go_bottom(),
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    fn draw(&mut self, terminal: &mut Terminal<impl Backend>) -> Result<(), Box<dyn Error>> {
+        terminal.draw(|f| f.render_widget(self, f.size()))?;
+        Ok(())
+    }
+}
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let vertical = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Min(0),
+            Constraint::Length(2),
+        ]);
+        let [header_area, rest_area, footer_area] = vertical.areas(area);
+
+        let horizontal =
+            Layout::horizontal([Constraint::Percentage(75), Constraint::Percentage(25)]);
+        let [left_area, right_area] = horizontal.areas(rest_area);
+
+        let vertical = Layout::horizontal([Constraint::Percentage(75), Constraint::Percentage(25)]);
+        let [graph_area, info_area] = vertical.areas(left_area);
+
+        let vertical = Layout::horizontal([Constraint::Length(2), Constraint::Min(0)]);
+        let [mode_area, list_area] = vertical.areas(right_area);
+
+        render_title(header_area, buf);
+        self.render_graph(graph_area, buf);
+        self.render_info(info_area, buf);
+        self.render_mode(mode_area, buf);
+        self.render_list(list_area, buf);
+        render_footer(footer_area, buf);
+    }
+}
+
+fn render_title(area: Rect, buf: &mut Buffer) {
+    Paragraph::new("Batt_log").centered().render(area, buf);
+}
+
+fn render_footer(area: Rect, buf: &mut Buffer) {
+    Paragraph::new("\nUse ↓↑ or j/k to move, g/G to go top/bottom.")
+        .centered()
+        .render(area, buf);
+}
--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+pub mod app;
+pub mod modules;
+pub mod terminal;
+use std::error::Error;
+
+use app::App;
+use terminal::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let terminal = init_terminal()?;
+
+    App::new().run(terminal)?;
+
+    restore_terminal()?;
+
+    Ok(())
 }
